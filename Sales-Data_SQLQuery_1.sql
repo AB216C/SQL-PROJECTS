ed43ad5d4b9a6b18d9b28ca714699585ff8feb2e
Ferdinand Niyongira <112482383+AB216C@users.noreply.github.com>
2023-06-11 14:56:27 -0500

    Add files via upload

--- Sales-Data_SQLQuery_1.sql
+++ Sales-Data_SQLQuery_1.sql
@@ -42,3 +42,38 @@ where YEAR_ID = 2004
 group by MONTH_ID 
 order by 2 desc 
 
+#--------Novermber Seems to be the best month IN 2004&2003 Which Productline was the best?
+select MONTH_ID, PRODUCTLINE, sum(SALES) as Revenue, count(ORDERLINENUMBER) as Frequency from dbo.Sales_Data 
+where YEAR_ID = 2004 and MONTH_ID = 11
+group by MONTH_ID,PRODUCTLINE
+order by 4 desc
+
+select MONTH_ID, PRODUCTLINE, sum(SALES) as Revenue, count(ORDERLINENUMBER) as Frequency from dbo.Sales_Data 
+where YEAR_ID = 2003 and MONTH_ID = 11
+group by MONTH_ID,PRODUCTLINE
+order by 4 DESC
+
+#-------Identifying the best customer using RFM(Recency= Last order date, Frequency = Total order, Monetary Value: Money spent)------#
+
+select customername,
+sum(sales) as Monetary_value,
+avg(sales) as Avgmonetary_value, 
+count(ordernumber) as Frequency,
+max(orderdate) as Last_order_date,
+(select max(orderdate) from dbo.Sales_Data) as max_order_date,
+DATEDIFF(DD, max(orderdate), (select max(orderdate) from dbo.Sales_Data) ) as recency
+from dbo.Sales_Data
+group by customername
+Order by recency desc
+
+#-------Identifying prodocuts shipped often together----------------#(2)
+
+select ordernumber from 
+    (select ordernumber, count(*) as record# from dbo.Sales_Data
+    where status = 'Shipped'
+    group by ordernumber) as m
+where record# = 3
+#--------Inspecting the previous results in (2)-------#
+select * from dbo.Sales_Data where ordernumber = 10365
+
+
